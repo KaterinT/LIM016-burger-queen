ef1e7db6115592582cf06028975b3d6b0de6b61a
unknown <cruzyumari2@gmail.com>
2022-02-25 00:31:52 -0500

    no vale este commit

---
+++ src/components/Cocinero/templatesCocinero.js
@@ -0,0 +1,20 @@
+  
+  export const TemplatePedidos = ({objeto, cambioEstado}) => {
+    
+    return <>
+            <div className="infoMesa"> <b>Mesa: </b>{objeto.mesa}</div>
+            <div className="infoPedido">
+                <p><b>Hora:</b> {objeto.hora}</p>
+                <p><b>Cliente: </b> {objeto.cliente}</p>
+                <ul>
+                  <b>Pedidos: </b>
+                  {objeto.pedidosArray.map((e) => {
+                    return (<li key={objeto.id+e.count+e.descripcion.replace(' ','')}>
+                              {e.count+''+e.descripcion}
+                            </li>)
+                  } )}
+                </ul>
+                {objeto.estado===false&&(<button onClick={() => cambioEstado(objeto.id)}>Listo?</button>)}
+            </div>
+           </>
+  }
---
+++ src/components/tomarOrden/modalExtras/modal.js
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+export const Modal = ({confirmarModal,cancelarModal,alarm,alarmSet}) => {
+
+    const [burger,setBurger] =useState('');
+    const [adicional,setAdicional] =useState('')
+
+    return <div className="modal">
+              <div className="modalOpciones">
+                <h3>Opciones de hamburguesa</h3>
+                <div>
+                    <label>
+                        <input type="radio" name="burger"  onChange={()=>{setBurger('res');alarmSet('noActive')}}/>
+                        Res    
+                    </label>
+                    <label>
+                        <input type="radio" name="burger"  onChange={()=>{setBurger('pollo');alarmSet('noActive')}} />Pollo</label>
+                    <label>
+                        <input type="radio" name="burger"  onChange={()=>{setBurger('vegana');alarmSet('noActive')}}/>Vegana</label>
+                </div>
+                {alarm==='isActive'&&(<p>Elegir hamburguesa por favor</p>)}
+                <h3>Adicionales</h3>
+                <div>
+                    <label><input type="radio" name="adicional" onChange={()=>setAdicional('queso')}/>Queso 1.00</label>
+                    <label><input type="radio" name="adicional" onChange={()=>setAdicional('huevo')}/>Huevo 1.00</label>
+                </div>
+                <input type="button" onClick={()=>confirmarModal([burger,adicional])} value="aceptar" className="bttn-Modal"/>
+                <input type="button" onClick={()=>{cancelarModal()}} value="x" className="closeModalBurger bttn-Modal"/>
+              </div>
+            </div>
+}
